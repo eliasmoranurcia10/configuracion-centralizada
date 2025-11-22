a5650550a641452ed59c1612fbada6c537c5d3ff
EliasMoran <yoexereliasmoranurcia@gmail.com>
2025-11-21 23:21:39 -0500

    first commit

---
+++ src/test/java/com/springcloud/configuracion_centralizada/ConfiguracionCentralizadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.configuracion_centralizada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiguracionCentralizadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
